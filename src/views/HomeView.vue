22062293293f910cd3473a2a7308be8a994f1197
JakubZojdzik <jakub.zojdzik@gmail.com>
2023-03-14 20:57:40 +0100

    Vue is not that easy xd

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+
 </script>
 
 <template>
     <main>
-        <TheWelcome />
+        <span style="font-size: 3em;">
+            mądrALO
+        </span>
+        <p>Zadania zadania, jeszcze nie ma zadań</p>
     </main>
 </template>
