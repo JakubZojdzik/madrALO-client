ef46d7eb18306992ce54fb35e425a696364d2eb2
JakubZojdzik <jakub.zojdzik@gmail.com>
2023-03-30 19:06:21 +0200

    Remove Rules temporary data

--- src/views/RulesView.vue
+++ src/views/RulesView.vue
@@ -1,19 +1,10 @@
 <script>
-import { UserTileItem } from '../components';
-
 export default {
-    components: {
-        UserTileItem
-    }
 };
 </script>
 
 <template>
     <main>
-        <UserTileItem name="Jan" email="jp2@papaj.com" position="{{1}}" points="{{2137}}" />
-        <UserTileItem name="PanJawel" email="jp2@papaj.com" position="{{2}}" points="{{2137}}" />
-        <UserTileItem name="Jan" email="jp2@papaj.com" position="{{3}}" points="{{2137}}" />
-        <UserTileItem name="Jan" email="jp2@papaj.com" position="{{4}}" points="{{2137}}" />
-        <ul></ul>
+        
     </main>
 </template>
