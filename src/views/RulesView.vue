74ac644f8c2e2b4c1714a474ac76f065606a3c62
JakubZojdzik <jakub.zojdzik@gmail.com>
2023-04-11 11:33:34 +0200

    Fill RulesView with content

--- src/views/RulesView.vue
+++ src/views/RulesView.vue
@@ -5,6 +5,105 @@ export default {
 
 <template>
     <main>
-        
+        <div class="wrapper">
+            <h2>Zasady:</h2>
+            <ul>
+                <li>Zadania należy rozwiązywać samodzielnie</li>
+                <li>Dozwolone jest kożystanie z internetu, komputerów, kalkulatorów itd.</li>
+                <li>Łamanie zasad skutkuje unieważnieniem uczestnictwa w konkursie</li>
+                <li>Wszystkie pytania dotycznące zadać lub systemu konkursowego powinny być kierowane na maila: jakub.zojdzik.2020@alo.pwr.edu.pl</li>
+            </ul>
+            <h2>Nagrody:</h2>
+            <ul>
+                <li>
+                    <span class="place1">Miejsce 1</span>:
+                    <ul>
+                        <li>
+                            Zwiedzanie <a href="https://pl.wikipedia.org/wiki/Bem_(superkomputer)" target="_blank" rel="noopener noreferrer">superkomputera Bem</a>
+                        </li>
+                        <li>
+                            Ocena celująca z matematyki
+                        </li>
+                    </ul>
+                </li>
+
+                <li>
+                    <span class="place2">Miejsce 2</span>:
+                    <ul>
+                        <li>
+                            Zwiedzanie <a href="https://pl.wikipedia.org/wiki/Bem_(superkomputer)" target="_blank" rel="noopener noreferrer">superkomputera Bem</a>
+                        </li>
+                        <li>
+                            Ocena celująca z matematyki
+                        </li>
+                    </ul>
+                </li>
+
+                <li>
+                    <span class="place3">Miejsce 3</span>:
+                    <ul>
+                        <li>
+                            Zwiedzanie <a href="https://pl.wikipedia.org/wiki/Bem_(superkomputer)" target="_blank" rel="noopener noreferrer">superkomputera Bem</a>
+                        </li>
+                        <li>
+                            Ocena celująca z matematyki
+                        </li>
+                    </ul>
+                </li>
+
+                <li>
+                    <span>Miejsce 4, 5, 6</span>
+                    <ul>
+                        <li>
+                            Plus z matematyki
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+
+            <p>Zwiedzanie superkomputera jest możliwe dzięki życzliwości Wrocławskiego Centrum Sieciowo-Superkomputerowego</p>
+        </div>
     </main>
 </template>
+
+<style scoped>
+.wrapper {
+    width: 80%;
+    margin-left: auto;
+    margin-right: auto;
+    line-height: 3rem;
+}
+
+a {
+    color: white;
+
+}
+
+span {
+    font-weight: bold;
+}
+
+.place1 {
+    color: #c9b037;
+}
+
+.place2 {
+    color: #b4b4b4;
+}
+
+.place3 {
+    color: #ad8a56;
+}
+</style>
+
+<!-- Communication throughout the CTF will be via Discord. For all ground truths regarding challenges, questions, and memes join the discord and ask an organizer (with the @org role).
+For the qualifying event team size is unlimited. For finals team size is limited to five people.
+To qualify as an academic team all five members sent to the finals event must be current undergraduates or highschool students and no older than 24 years of age.
+Academic teams may bring a sixth person as a mentor, there are no education or age restrictions for this person. This person may assist the team but not directly play.
+The top 8 Academic teams (that meet the "Viking Rule" criteria) and the top 8 Professional teams will qualify for the in-person finals event.
+If you need to cheat, you shouldn’t be here, immediate disqualification. Our qualifying event is easy, only skilled people will have a chance to succeed at the finals.
+Would you kick a puppy? Steal candy from a baby? Harming infra is just as bad, immediate disqualification.
+Bullying of all kinds is not permitted. Sexual harrasment of any kind is not permitted. Any violations will result in a team's immediate disqualification.
+Writeups may be requested from winning teams to attend the finals event.
+If you have any issues contact an organizer.
+All teams are required to have fun. Apply alcohol liberally (if of age). -->
