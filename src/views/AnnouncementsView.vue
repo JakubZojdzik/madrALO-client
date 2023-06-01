11d1eab80d23ab02ae416fc8bb7812b49ec12140
JakubZojdzik <jakub.zojdzik@gmail.com>
2023-06-01 22:55:59 +0200

    add tmp values

--- src/views/AnnouncementsView.vue
+++ src/views/AnnouncementsView.vue
@@ -1,19 +1,29 @@
 <script>
-// import { EditorItem } from '../components';
-// import axios from 'axios';
-// import VueCookie from 'vue-cookie';
+import axios from 'axios';
 
-// const url = import.meta.env.VITE_APP_API_URL;
+const url = import.meta.env.VITE_APP_API_URL;
 
 export default {
-    data() {},
-    methods: {}
+    data() {
+        return {
+            ann: null
+        };
+    },
+    methods: {
+        async fetchData() {
+            this.ann = (await axios.get(url + '/challenges/inactiveChallenges')).data;
+        }
+    },
+    created() {
+        this.fetchData();
+    }
 };
 </script>
 
 <template>
     <main>
-        <div>papiez</div>
+        papiez
+        <p v-for="{ id, title, content, author, added } in ann" :key="id">uuu{{ title }}: {{ content }}, {{ author }}, {{ added }}</p>
     </main>
 </template>
 
