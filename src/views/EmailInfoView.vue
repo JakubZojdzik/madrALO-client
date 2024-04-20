322ee860775258e6c52b3f61e16e9c0e90e8fbce
JakubZojdzik <jakub.zojdzik@gmail.com>
2024-04-20 09:23:10 +0200

    Change welcome message

--- src/views/EmailInfoView.vue
+++ src/views/EmailInfoView.vue
@@ -5,8 +5,7 @@
         <div class="wrapper">
             <h1>Dziękuję za rejestracje!</h1>
             <p>Na twojego maila została wysłana wiadomość weryfikacyjna. Aby aktywować swoje konto otwórz link znajdujący się w wiadomości.</p>
-            <p>Na aktywację konta masz 15min. Jeśli nie zdążyłeś tego zrobić, mail został nie wysłany lub konto nie zostało aktywowane skontaktuj się ze mną:</p>
-            <p>jakub.zojdzik.2020@alo.pwr.edu.pl</p>
+            <p>Jeśli masz problem z rejestracją, skontaktuj się z administracją</p>
         </div>
     </main>
 </template>
